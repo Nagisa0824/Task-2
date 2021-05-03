f5fff0a245b35619c353f57fef713bc190e0fbc5
Nagisa0824 <yuzutea0824@gmail.com>
2021-05-03 13:05:45 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+end
