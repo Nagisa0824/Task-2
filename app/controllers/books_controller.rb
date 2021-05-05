f0dc8ae059c10f26fa9033674e5936e0acae4c29
Nagisa0824 <yuzutea0824@gmail.com>
2021-05-05 13:40:51 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,14 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @books = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user =  @user = current_user
   end
 
   def create
@@ -15,7 +19,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -43,7 +47,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     @books = @user.books
     @book = Book.new
   end
+  
 
   def index
     @users = User.all
