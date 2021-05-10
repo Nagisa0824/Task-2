da47c3c6e8809350323a37f1b143b7b4d022d284
Nagisa0824 <yuzutea0824@gmail.com>
2021-05-10 14:47:57 +0000

    Initial Commit

---
+++ app/models/relationship.rb
@@ -0,0 +1,6 @@
+class Relationship < ApplicationRecord
+    
+    belongs_to :follower, class_name: "User"
+ 　 belongs_to :followed, class_name: "User"
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
   
 end 
 
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+  resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+end 
+
 end
