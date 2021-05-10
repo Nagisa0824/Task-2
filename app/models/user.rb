535c7eb0a8b673d55c93f63b6f9a279b72437d8f
Nagisa0824 <yuzutea0824@gmail.com>
2021-05-10 15:32:48 +0000

    Initial Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
 
-  has_many :books
-  has_many :favorites, dependent: :destroy
-  has_many :book_comments, dependent: :destroy
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followers, through: :reverse_of_relationships, source: :follower
-  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-   has_many :followings, through: :relationships, source: :followed
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
@@ -20,6 +14,14 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+  has_many :books
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
